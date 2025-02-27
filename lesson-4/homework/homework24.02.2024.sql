2a4c0bebab6a27df2d6d38a2320283e1bc84c306
ABA-0204 <aba8504@gmail.com>
2025-02-27 15:48:36 +0500

    Update homework24.02.2024.sql

--- lesson-4/homework/homework24.02.2024.sql
+++ lesson-4/homework/homework24.02.2024.sql
@@ -13,5 +13,20 @@ Select top 10 * from Products order by Price desc
 Select coalesce (FirstName,LAstName) from Employees
 Select distinct (Category,Price) from Products 
 Select * from Employees where (Age between 30 and 40) or Department='Marketing'
-Select * from Employees order by Salary desc offset 10 rows fetch next 10 
-ghfhfg
+Select * from Employees order by Salary desc offset 10 rows fetch next 10 rows only
+Select * from Products where price<=1000 and Stock>50 order by stock asc
+Select * from Products where ProductName like '%e%'
+Select * from Employees where department in ('HR', 'IT', 'Finance')
+Select * from Employees where salary> any (select avg salary from Employees)
+Select * from Customers order by city asc and PostalCode desc
+
+Select top10 * from Products order by SalesAmount desc
+Select coalesce (FirstName, LastName, 'FullName') from Employees
+Select distinct (Category, ProductName, Price) when price > 50 from Products
+Select top 10 percent *  from Products order by avg(price)
+Select * from Employees where age <30 and Department='HR' or Department='IT'
+Select * from Customers where email like'%@gmail.com'
+Select * from Employees where salary> all (select salary from Employees where department='Sales')
+Select * from Employees order by salary desc offset 0 rows fetch next 10 rows only
+Select orders from Orders where placed_date BETWEEN (2024-01-27) and (CURRENT_DATE)
+Select * from Employee where salary> any (select avg salary from Employee group by department)
