10349a18fe9017d919e214906d27151742d188c4
ABA-0204 <aba8504@gmail.com>
2025-03-10 16:59:05 +0500

    Create homework07-03-2025.sql

---
+++ lesson-9/homework/homework07-03-2025.sql
@@ -0,0 +1,4 @@
+SELECT * FROM Employees JOIN Departments ON Salary>5000
+SELECT * FROM Customers JOIN Orders ON OrderDate 2023
+SELECT * FROM Employees E LEFT JOIN Departments D 
+ON E.DepartmentID=D.DepartmentID
